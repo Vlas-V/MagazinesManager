e736bcf1aa7a31295b7e0310d517eddd724be6bb
Vlas Novokhatnii <VlasNovokhatnii@outlook.com>
2019-05-19 09:42:48 +0300

    MagazineCollections class has been created and sorting method have been added to it.

---
+++ MagazinesManager/MagazineCollection.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MagazinesManager
+{
+    public class MagazineCollection
+    {
+        private List<Magazine> magazines = new List<Magazine>();
+
+        public List<Magazine> Magazines
+        {
+            // Reference of value?
+            get { return magazines; }
+        }
+
+
+        void AddDefaults()
+        {
+            throw new NotImplementedException();
+        }
+
+        void AddMagazines(params Magazine[] mags)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+        public string ToShortString()
+        {
+            return base.ToString();
+        }
+
+
+        // Methods for sorting
+
+        void SortByName()
+        {
+            Magazines.Sort();
+        }
+
+        void SortByPublicationDate()
+        {
+            Magazines.Sort((IComparer<Edition>)new Magazine());
+        }
+
+        void SortByCirculation()
+        {
+            Magazines.Sort(Magazine.SortByCirculation);
+        }
+
+
+    }
+}
